03049bd52f448cea1ae35c2a9e42da01ace08896
Edgarwebb <161100457+Edgarwebb@users.noreply.github.com>
2024-07-08 14:49:36 -0600

    3

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
 import { FormComponent } from './form/form.component';
-import { RouterOutlet } from '@angular/router';
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet,FormComponent],
+  imports: [FormComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
